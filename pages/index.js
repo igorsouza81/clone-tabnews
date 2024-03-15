1bf0a85af30e72e27ba859a064ad116159c20875
Igor Souza <55673860+igorsouza81@users.noreply.github.com>
2024-03-15 02:03:55 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Lindona, eu amo você. Se você me ama, dá uma risadinha! xD </h1>
+  return <h1>Lindona, eu amo você. Se você me ama, dá uma risadinha! xD </h1>;
 }
 
 export default Home;
